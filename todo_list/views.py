fc4dfee2e77da26919108241776b36ea26cefee4
Assentt <tubbeethan@gmail.com>
2023-03-06 18:41:19 -0800

    formatting and readability

--- todo_list/views.py
+++ todo_list/views.py
@@ -10,19 +10,23 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
+
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
 
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
+
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+    
 
 class TaskDelete(DeleteView):
     model = Task
